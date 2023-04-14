6ae931afc707d04bd19279ee1e2b72c6acf032c7
NSM239 <123148583+NSM239@users.noreply.github.com>
2023-04-14 01:52:15 -0500

    file

--- controllers/boats.js
+++ controllers/boats.js
@@ -33,7 +33,7 @@ exports.boats_create_post = async function(req, res) {
     // We are looking for a body, since POST does not have query parameters. 
     // Even though bodies can be in many different formats, we will be picky 
     // and require that it be a json object 
-    //{"costume_type":"goat", "cost":12, "size":"large"} 
+    //{"boats_type":"goat", "cost":12, "size":"large"} 
     //{"BoatType":"Sail powered boats", "BoatsCost":5000, "Capacity":"420", "Hull":"V-Shaped Hulls"}
     document.BoatType = req.body.BoatType; 
     document.BoatsCost = req.body.BoatsCost; 
@@ -93,3 +93,34 @@ exports.boats_view_all_Page = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 };
+exports.boats_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await boats.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+
+exports.boats_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await boats.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.boats_color)
+    toUpdate.BoatType = req.body.BoatType;
+    if(req.body.BoatsCost) toUpdate.BoatsCost = req.body.BoatsCost;
+    if(req.body.Capacity) toUpdate.Capacity = req.body.Capacity;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };    
+        
